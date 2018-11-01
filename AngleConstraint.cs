6218ebe65cfa863271af13ebc32bf420400d9c53
Qin Jia <hzqinjia@corp.netease.com>
2018-11-01 20:31:07 +0800

    First rough version of angle constraint.

--- AngleConstraint.cs
+++ AngleConstraint.cs
@@ -7,8 +7,9 @@ public class AngleConstraint : MonoBehaviour {
 	[HideInInspector]
 	[SerializeField]
 	private Transform _childJoint = null;
-	// public properties are not serialized, only public fields are
+	// Public properties are not serialized, only public fields are
 	// serialized. Properties are actually methods.
+	// This is for making undo work properly.
 	public Transform ChildJoint{
 		get{
 			return _childJoint;
@@ -21,12 +22,11 @@ public class AngleConstraint : MonoBehaviour {
 		}
 	}
 
-	[Range(0, 360)]
-	public float rotAxisAdjustAngle= 0.0f;
-
 	// all axes coordinates are in local space
 	public Vector3 twistAxis = Vector3.forward;
 	public Vector3 rotateAxis = Vector3.right;
+
+	private Quaternion _prevRotation;
 
 	void Reset (){
 		// Reset() is called when script is attached.
@@ -39,43 +39,68 @@ public class AngleConstraint : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		twistAxis = (_childJoint.position - transform.position).normalized;
-
-		// get the twistAxis without rotation
-		twistAxis = Quaternion.Inverse(transform.rotation) * twistAxis;
-
+		_prevRotation = transform.localRotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = ApplyConstraint(transform.rotation);
-
+		ApplyConstraint();
+		_prevRotation = transform.localRotation;
 	}
 
-	public Quaternion ApplyConstraint(Quaternion q){
-		// The idea is to decompose rotation q into two quaternions.
-		// One swing twist axis from old direction to new direction. 
-		// The other represent rotation around twistAxis.
-		// let q = swingQ * twistQ;
-		// therefore, twistQ = reverse(swingQ) * q;
-		Quaternion swingQ, twistQ;
+	public void ApplyConstraint(){
+		// Let qNew = qOld * deltaQ, so deltaQ = inverse(qOld) * qNew
+		// The reason why we are not using qNew = deltaQ * qOld, is because
+		// in the first equation, deltaQ gets applied first, so everyting is
+		// in local space without any rotation. It's easier for us to
+		// construct the replacement of deltaQ.
+		Quaternion deltaQ = Quaternion.Inverse(_prevRotation) * transform.localRotation;
+		Vector3 newTwistAxis = deltaQ * twistAxis;
 
-		Vector3 newTwistAxis = q * twistAxis;
+		//Quaternion swingQ, twistQ;
+		//DecomposeRotation(deltaQ, twistAxis, out swingQ, out twistQ);
 
-		swingQ = Quaternion.FromToRotation(twistAxis, newTwistAxis);
-		twistQ = Quaternion.Inverse(swingQ) * q;
-
-		Debug.Log("q: " + q);
-		Debug.Log("swing: " + swingQ);
-		Debug.Log("twistQ: " + twistQ);
-
-		return swingQ * twistQ;
+		// transform.localRotation = swingQ * twistQ * _prevRotation;
 
 		// apply constraint
-		// project 
+		// project twist axis before and after rotation on the plane
+		// perpendicular to rotation axis.
+		Vector3 projTwistAxis = ProjectDirection(twistAxis, rotateAxis);
+		Vector3 projNewTwistAxis = ProjectDirection(newTwistAxis, rotateAxis);
+		Debug.Log(Vector3.Dot(projNewTwistAxis, rotateAxis));
+		Debug.Log(Vector3.Dot(projTwistAxis, rotateAxis));
+
+		//Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(projTwistAxis));
+		//Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(projNewTwistAxis), Color.green);
+
+		Quaternion newSwingQ = Quaternion.FromToRotation(projTwistAxis, projNewTwistAxis);
+
+		transform.localRotation = _prevRotation * newSwingQ;
 	}
 
+	private void DecomposeRotation(Quaternion q, Vector3 axis,
+		out Quaternion swing, out Quaternion twist){
+		// Decompose rotation q into two quaternions.
+		// One swing axis from old direction to new direction. 
+		// The other represent twist rotation around axis.
+		// let q = swingQ * twistQ;
+		// therefore, twistQ = inverse(swingQ) * q;
+		Vector3 newAxis = q * axis;
+		swing = Quaternion.FromToRotation(axis, newAxis);
+		twist = Quaternion.Inverse(swing) * q;
+	}
 
+	private Vector3 ProjectDirection(Vector3 dir, Vector3 axis ){
+		// Project dir to the plane perpendicular to the axis.
+		dir.Normalize();
+		axis.Normalize();
+
+		// componet parallel to axis.
+		Vector3 d1 = Vector3.Dot(dir, axis) * axis;
+
+		// componet perpendicular to axis.
+		return  (dir - d1).normalized;
+	}
 
 	void CalculateRotationAxis(){
 		// rotation axis and twist axis should alway be perpendicular to
@@ -83,38 +108,5 @@ public class AngleConstraint : MonoBehaviour {
 		twistAxis = (_childJoint.position - transform.position).normalized;
 
 		rotateAxis = new Vector3(twistAxis.y, twistAxis.x, 0.0f);
-
-
-
-	}
-
-
-	void OnDrawGizmosSelected(){
-		// draw twist axis
-		//Gizmos.color = Color.yellow;
-		//Gizmos.DrawLine(transform.position, childJoint.position);
-
-		// draw rotation axis
-		//Gizmos.color = Color.red;
-		//Gizmos.DrawLine(transform.position - rotateAxis, 
-			//transform.position + rotateAxis);
-
-		//Vector3 rotateAxisWorld = transform.TransformDirection(rotateAxis);
-		//Gizmos.DrawLine(
-			//transform.position + rotateAxisWorld,
-			//transform.position - rotateAxisWorld); 
-		
-		//Gizmos.color = Color.red;
-		//Vector3 axis;
-		//transform.rotation.ToAngleAxis(out angle, out axis);
-		//Gizmos.DrawLine(transform.position, transform.position + axis);
-
-		//Gizmos.color = Color.blue;
-		//Vector3 twistAxisWorld = transform.TransformDirection(twistAxis);
-		//Gizmos.DrawLine(transform.position, transform.position + twistAxisWorld);
-
-		//Quaternion baseQ = Quaternion.FromToRotation(Vector3.forward, rotateAxis);
-		//transform.localRotation = baseQ * Quaternion.AngleAxis(rotateAngle, twistAxis);
-
 	}
 }
