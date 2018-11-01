ff1630c9540a2b4cb29d3cc1c936c798ec27d093
Qin Jia <hzqinjia@corp.netease.com>
2018-11-01 15:14:43 +0800

    Allow editing twist and rotate axis.

--- AngleConstraintEditor.cs
+++ AngleConstraintEditor.cs
@@ -16,64 +16,95 @@ public class AngleConstraintEditor : Editor{
 	bool _editingTwistAxis = false;
 	bool _showAxis = true;
 
+	// This is for tracking the rotation of axis
+	// when editing it using Unity's rotate handle.
+	Quaternion _twistAxisQ;
+	Quaternion _rotateAxisQ;
+
+	// record the axes corresponding _twistAxisQ and _rotateAxisQ
+	Vector3 _savedTwistAxis;
+	Vector3 _savedRotateAxis;
+
 	void OnEnable(){
+		AngleConstraint constraint = target as AngleConstraint;
+
 		// Set up serialized properties.
 		// Accessing properties through serialized properties rather through
 		// target is because undo, multi-editing is handled automatically.
 		_rotateAxisP = serializedObject.FindProperty("rotateAxis");
 		_twistAxisP = serializedObject.FindProperty("twistAxis");
+
+		_rotateAxisQ = constraint.transform.rotation;
+		_twistAxisQ = constraint.transform.rotation;
+		_savedRotateAxis = constraint.rotateAxis;
+		_savedTwistAxis = constraint.twistAxis;
+	}
+
+	void ShowAxis(AngleConstraint constraint){
+		// draw rotation axis
+		Vector3 axis = constraint.transform.TransformDirection(
+			constraint.rotateAxis);
+		Handles.color = Color.red;
+		Handles.DrawLine(constraint.transform.position-axis,
+			constraint.transform.position+axis);
+
+		// draw twist axis
+		axis = constraint.transform.TransformDirection(
+			constraint.twistAxis);
+		Handles.color = Color.yellow;
+		Handles.DrawLine(constraint.transform.position,
+			constraint.transform.position+axis);
+	}
+
+	void EditTwistAxis(AngleConstraint constraint){
+		EditorGUI.BeginChangeCheck();
+		//_twistAxisQ = Handles.RotationHandle(_twistAxisQ, constraint.transform.position);
+
+		// Let q2 = Handles.RotationHandle(q1);
+		// q2 is not the delta rotation between q1 and q2. q2 is the modified
+		// rotation of q1.
+		_twistAxisQ = Handles.RotationHandle(_twistAxisQ, constraint.transform.position);
+
+		if (EditorGUI.EndChangeCheck()){
+			Undo.RecordObject(constraint, "Rotate Twist Axis");
+			constraint.twistAxis = Quaternion.Inverse(constraint.transform.rotation) * _twistAxisQ * _savedTwistAxis;
+		}
+	}
+
+	void EditRotateAxis(AngleConstraint constraint){
+		EditorGUI.BeginChangeCheck();
+		_rotateAxisQ = Handles.RotationHandle(_rotateAxisQ, constraint.transform.position);
+
+		if (EditorGUI.EndChangeCheck()){
+			Undo.RecordObject(constraint, "Rotate Rotation Axis");
+			constraint.rotateAxis= Quaternion.Inverse(constraint.transform.rotation) * _rotateAxisQ * _savedRotateAxis;
+		}
 	}
 
 	public void OnSceneGUI(){
 		AngleConstraint constraint = target as AngleConstraint;
 
 		if(_showAxis){
-			// draw rotation axis
-			Vector3 axis = constraint.transform.TransformDirection(
-				constraint.rotateAxis);
-			Handles.color = Color.red;
-			Handles.DrawLine(constraint.transform.position-axis,
-				constraint.transform.position+axis);
-
-			// draw twist axis
-			axis = constraint.transform.TransformDirection(
-				constraint.twistAxis);
-			Handles.color = Color.yellow;
-			Handles.DrawLine(constraint.transform.position,
-				constraint.transform.position+axis);
+			ShowAxis(constraint);
 		}
 
 		// enable twist axis editing
 		if(_editingTwistAxis){
-			EditorGUI.BeginChangeCheck();
-			Quaternion q = Handles.RotationHandle(constraint.transform.rotation, constraint.transform.position);
-			q = q * Quaternion.Inverse(constraint.transform.rotation);
-
-			if (EditorGUI.EndChangeCheck()){
-				Undo.RecordObject(constraint, "Rotate Twist Axis");
-				constraint.twistAxis = q * constraint.twistAxis; 
-			}
+			EditTwistAxis(constraint);
 		}
 
 		// enable rotation axis editing
 		if(_editingRotateAxis){
-			EditorGUI.BeginChangeCheck();
-			Quaternion q = Handles.RotationHandle(
-				constraint.transform.rotation, constraint.transform.position);
-
-			if (EditorGUI.EndChangeCheck()){
-				Undo.RecordObject(constraint.transform, "Custom Rotate");
-				constraint.transform.rotation = q;
-			}
+			EditRotateAxis(constraint);
 		}
-
 	}
 
 	public override void OnInspectorGUI(){
+		AngleConstraint constraint = target as AngleConstraint;
+
 		// toggle to show axis
 		_showAxis = EditorGUILayout.Toggle("Show Axis", _showAxis);
 
-		AngleConstraint constraint = target as AngleConstraint;
 		// Since ChildJoint is a property (actually a method) of AngleConstraint,
 		// it cannot be serialized. So we have to access it through target.
 		// The bad thing is we have to do all the undo dirty stuff on our own.
