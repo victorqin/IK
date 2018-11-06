92e7b87b6dd806b9580debcfbde4145434ae76a6
Qin Jia <hzqinjia@corp.netease.com>
2018-11-06 18:13:43 +0800

    Rewrite angle constraint (renamed as HingeConstraint).

---
+++ Constraints/HingeConstraint.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HingeConstraint : MonoBehaviour {
+	public bool autoUpdate = true;
+
+	// rotation axis
+	private Vector3 _rotationAxis = Vector3.forward;
+	private Vector3 _crossAxis = Vector3.up;	// where 0 degree starts
+	public Vector3 rotationAxis{
+		get{
+			return _rotationAxis;
+		}
+		set{
+			_rotationAxis  = value;
+
+			// Get an arbitary vector, but make sure it's not equal to _rotationAxis.
+			Vector3 v = new Vector3(_rotationAxis.z, _rotationAxis.x, _rotationAxis.y);
+			if (v == _rotationAxis){
+				v = Vector3.up;
+			}
+				_crossAxis = Vector3.Cross(v, _rotationAxis).normalized;
+		}
+	}
+	public Vector3 crossAxis{
+		get{
+			return _crossAxis;
+		}
+	}
+
+	// use min and max rotation angle limit around rotation axis
+	public bool useAngleLimits = true;
+
+	// range of _minAngle, _maxAngle is [-180, 180]
+	[SerializeField]
+	private float _minAngle = -90.0f;
+	[SerializeField]
+	private float _maxAngle = 90.0f;
+	public float minAngle{
+		get{
+			return _minAngle;
+		}
+		set{
+			value = NormalizeAngle(value);
+			if(value > _maxAngle){
+				Debug.LogError("minAngle should NOT be greater than maxAngle.");
+				return;
+			}
+			_minAngle = value;
+		}
+	}
+	public float maxAngle{
+		get{
+			return _maxAngle;
+		}
+		set{
+			value = NormalizeAngle(value);
+			if(value < _maxAngle){
+				Debug.LogError("maxAngle should NOT be smaller than minAngle.");
+				return;
+			}
+			_maxAngle = NormalizeAngle(value);
+		}
+	}
+
+	// default local rotation
+	private Quaternion _defaultLocalRotation;
+	private bool _defaultLocalRotationSet = false;
+	public Quaternion defaultLocalRotation{
+		get{
+			if(!_defaultLocalRotationSet){
+				SetDefaultLocalRotation();
+			}
+			return _defaultLocalRotation;
+		}
+		set{
+			_defaultLocalRotation = value;
+			_defaultLocalRotationSet = true;
+		}
+	}
+
+	// Set the default rotation of the object.
+	// All rotation constraint is based on the default rotation.
+	private void SetDefaultLocalRotation(){
+		_defaultLocalRotation = transform.localRotation;
+		_defaultLocalRotationSet = true;
+	}
+
+	void Awake() {
+		SetDefaultLocalRotation();
+	}
+
+	// Update is called once per frame
+	void LateUpdate() {
+		if(autoUpdate){
+			transform.localRotation = ApplyConstraint(transform.localRotation);
+		}
+	}
+
+	// Apply constraint to rotation.
+	// Input is the new local rotation.
+	// Return true if rotation is modified due to the constraint.
+	// Make sure rotation is in local space.
+	public Quaternion ApplyConstraint(Quaternion localRotation){
+		Quaternion deltaLocalRotation =
+			Quaternion.Inverse(defaultLocalRotation) * localRotation;
+		Quaternion limitedDeltaLocalRotation = LimitHinge(deltaLocalRotation);
+		return defaultLocalRotation * limitedDeltaLocalRotation;
+	}
+
+	public Quaternion ApplyConstraint(Quaternion localRotation, out bool isChanged){
+		Quaternion q = ApplyConstraint(localRotation);
+		if (q == localRotation){
+			isChanged = false;
+		}else{
+			isChanged = true;
+		}
+		return q;
+	}
+
+	// Make sure rotation is in local space.
+	// This is because _rotationAxis is in local space.
+	private Quaternion LimitHinge(Quaternion localRotation){
+		Quaternion newRotation = ConstraintRotation1DOF(localRotation, _rotationAxis);
+
+		if(useAngleLimits){
+			Vector3 crossAxisNew = newRotation * _crossAxis;
+
+			// Vector3.SignedAngle makes sure the angle returns falls in range
+			// of [-180, 180].
+			float angle = Vector3.SignedAngle(_crossAxis, crossAxisNew, _rotationAxis);
+			angle = Mathf.Clamp(angle, _minAngle, _maxAngle);
+			return Quaternion.AngleAxis(angle, _rotationAxis);
+		}else{
+			return newRotation;
+		}
+	}
+
+	// normalize angle to [-180, 180]
+	private float NormalizeAngle(float angle){
+		while(angle<= -180.0f){
+			angle += 360.0f;
+		}
+		while(angle>= 180.0f){
+			angle -= 360.0f;
+		}
+		return angle;
+	}
+
+	// Constraint rotation to 1 DOF around given axis.
+	private Quaternion ConstraintRotation1DOF(Quaternion rotation, Vector3 axis){
+		// Let rotation = swing * twist. twist is the rotation we want to get.
+		// twist = inverse(swing) * rotation
+		// FromToRotation(rotation*axis, axis) is actually inverse(swing).
+		return Quaternion.FromToRotation(rotation * axis, axis) * rotation;
+	}
+}
---
+++ Constraints/HingeConstraintEditor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(HingeConstraint))]
+public class HingeConstraintEditor : Editor{
+
+	private HingeConstraint _constraint = null;
+	public HingeConstraint constraint{
+		get{
+			if(_constraint == null)
+				_constraint = target as HingeConstraint;
+			return _constraint;
+		}
+	}
+
+	void OnSceneGUI(){
+		if(!Application.isPlaying){
+			// when not in running mode, object's current local rotation is
+			// the default local rotation.
+			constraint.defaultLocalRotation = constraint.transform.localRotation;
+		}
+		DrawAxes();
+	}
+
+	public override void OnInspectorGUI(){
+
+	}
+
+	private void DrawAxes(){
+		Vector3 p = constraint.transform.position;
+		Vector3 axis = constraint.transform.TransformDirection(constraint.rotationAxis).normalized;
+
+		Vector3 crossAxis = ToWorldSpace(constraint.crossAxis);
+
+		Vector3 minDir = ToWorldSpace(Quaternion.AngleAxis(constraint.minAngle, constraint.rotationAxis) * constraint.crossAxis);
+		Vector3 maxDir = ToWorldSpace(Quaternion.AngleAxis(constraint.maxAngle, constraint.rotationAxis) * constraint.crossAxis);
+
+		Color c1 = new Color(0.667f, 1.0f, 0.0f, 1.0f);
+		Color c2 = new Color(0.667f, 1.0f, 0.0f, 0.1f);
+
+		Handles.color = c1;
+		Handles.DrawWireDisc(p, axis, 0.5f);
+
+		DrawArrow(p, axis*0.75f, c1, "Axis");
+		DrawArrow(p, crossAxis*0.5f, c1, "0");
+		DrawArrow(p, minDir*0.75f, c1, "Min");
+		DrawArrow(p, maxDir*0.75f, c1, "Max");
+
+		if(Application.isPlaying){
+			Vector3 newCrossAxis = constraint.transform.TransformDirection(constraint.crossAxis);
+			DrawArrow(p, newCrossAxis*0.75f, c1, "Current");
+		}
+
+		Handles.color = c2;
+		Handles.DrawSolidArc(
+			p, axis, minDir, constraint.maxAngle - constraint.minAngle, 0.5f);
+	}
+
+	private void DrawArrow(Vector3 position, Vector3 direction, Color color,
+		string label = "", float size = 0.02f){
+		Handles.color = color;
+		Handles.DrawLine(position, position + direction);
+		Handles.SphereHandleCap(0, position + direction, Quaternion.identity, size, EventType.Repaint);
+
+		if(label != ""){
+			GUIStyle style = new GUIStyle();
+			style.normal.textColor = color;
+			Handles.Label(position+direction, label, style);
+		}
+	}
+
+	private Vector3 ToWorldSpace(Vector3 v){
+		if(constraint.transform.parent){
+			return constraint.transform.parent.rotation * constraint.defaultLocalRotation * v;
+		}else{
+			return constraint.defaultLocalRotation * v;
+		}
+	}
+}
